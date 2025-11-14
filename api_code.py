63faac6fcc4e71c7860e4e782ce35718ece6ea4c
zahra <zsy.1414@gmail.com>
2025-11-14 11:58:26 +0330

    remove wrong entrypoints, set correct fastapi app

--- api_code.py
+++ api_code.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"status": "ok", "message": "Dashboard API is running!"}
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/api/dashboard")
-def dashboard():
-    return {"dashboard": "this is dashboard endpoint!"}
+@app.get("/")
+def get_dashboard():
+    return {
+        "summary": {
+            "totalEvents": 6,
+            "wellCount": 6,
+            "anomalyRate": 0.46,
+            "avgPressure": 78.9
+        },
+        "status": "OK"
+    }
